a17772cb3c53c06e250e56bf17ecab751bf5b580
Maciej Maćkowski <mmackows@sigma.ug.edu.pl>
2011-07-25 18:00:37 +0200

    Day 4

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,16 +12,17 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')
+    $this->jobeet_jobs = Doctrine::getTable('JobeetJob')
       ->createQuery('a')
       ->execute();
   }
 
+
   public function executeShow(sfWebRequest $request)
-  {
-    $this->jobeet_job = Doctrine_Core::getTable('JobeetJob')->find(array($request->getParameter('id')));
-    $this->forward404Unless($this->jobeet_job);
-  }
+{
+  $this->job = Doctrine::getTable('JobeetJob')-> find($request->getParameter('id'));
+  $this->forward404Unless($this->job);
+}
 
   public function executeNew(sfWebRequest $request)
   {
