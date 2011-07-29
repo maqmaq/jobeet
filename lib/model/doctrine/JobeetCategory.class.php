9e6634a05e1ac10bbdaa3cbe1726522a82407424
Maciej Maćkowski <mmackows@sigma.ug.edu.pl>
2011-07-29 22:05:56 +0200

    Day 7

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -13,14 +13,29 @@
 class JobeetCategory extends BaseJobeetCategory
 {
 	
-public function getActiveJobs($max = 10)
+	public function getActiveJobsQuery()
 {
   $q = Doctrine_Query::create()
     ->from('JobeetJob j')
-    ->where('j.category_id = ?', $this->getId())
+    ->where('j.category_id = ?', $this->getId());
+ 
+  return Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
+}
+	
+public function getActiveJobs($max = 10)
+{
+  $q = $this->getActiveJobsQuery()
     ->limit($max);
  
-  return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+  return $q->execute();
 }
+ 
+public function countActiveJobs()
+{
+  return $this->getActiveJobsQuery()->count();
+}
+
+
+
 
 }
