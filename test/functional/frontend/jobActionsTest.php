c58e28776f5087f0ea3577baa641cd3c65f6b894
Maciej Maćkowski <mmackows@sigma.ug.edu.pl>
2011-08-03 19:39:24 +0200

    Day 8 + 9

--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -1,19 +1,74 @@
 <?php
 
+// test/functional/frontend/jobActionsTest.php
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
-
-$browser = new sfTestFunctional(new sfBrowser());
-
-$browser->
-  get('/job/index')->
-
+ 
+$browser = new JobeetTestFunctional(new sfBrowser());
+$browser->loadData();
+ 
+$browser->info('1 - The homepage')->
+  get('/')->
   with('request')->begin()->
     isParameter('module', 'job')->
     isParameter('action', 'index')->
   end()->
-
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    info('  1.1 - Expired jobs are not listed')->
+    checkElement('.jobs td.position:contains("expired")', false)->
   end()
 ;
+
+$max = sfConfig::get('app_max_jobs_on_homepage');
+ 
+$browser->info('1 - The homepage')->
+  get('/')->
+  info(sprintf('  1.2 - Only %s jobs are listed for a category', $max))->
+  with('response')->
+    checkElement('.category_programming tr', $max)
+;
+
+$browser->info('1 - The homepage')->
+  get('/')->
+  info('  1.3 - A category has a link to the category page only if too many jobs')->
+  with('response')->begin()->
+    checkElement('.category_design .more_jobs', false)->
+    checkElement('.category_programming .more_jobs')->
+  end()
+;
+
+$q = Doctrine_Query::create()
+  ->select('j.*')
+  ->from('JobeetJob j')
+  ->leftJoin('j.JobeetCategory c')
+  ->where('c.slug = ?', 'programming')
+  ->andWhere('j.expires_at > ?', date('Y-m-d', time()))
+  ->orderBy('j.created_at DESC');
+ 
+$job = $q->fetchOne();
+ 
+$browser->info('1 - The homepage')->
+  get('/')->
+  info('  1.4 - Jobs are sorted by date')->
+  with('response')->begin()->
+    checkElement(sprintf('.category_programming tr:first a[href*="/%d/"]', $browser->getMostRecentProgrammingJob()))->
+  end()
+;
+
+$job = $browser->getMostRecentProgrammingJob();
+ 
+$browser->info('2 - The job page')->
+  get('/')->
+ 
+  info('  2.1 - Each job on the homepage is clickable and give detailed information')->
+  click('Web Developer', array(), array('position' => 1))->
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'show')->
+    isParameter('company_slug', $job->getCompanySlug())->
+    isParameter('location_slug', $job->getLocationSlug())->
+    isParameter('position_slug', $job->getPositionSlug())->
+    isParameter('id', $job->getId())->
+  end()
+;
+
+?>
