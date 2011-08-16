8c6b59a2a9cb707a7cb4fe23d7998c89f1351584
Maciej Maćkowski <mmackows@sigma.ug.edu.pl>
2011-08-16 23:49:00 +0200

    Day 12

--- lib/form/doctrine/JobeetJobForm.class.php
+++ lib/form/doctrine/JobeetJobForm.class.php
@@ -3,6 +3,8 @@ class JobeetJobForm extends BaseJobeetJobForm
 {
   public function configure()
   {
+	$this->removeFields();
+	    
     unset(
       $this['created_at'], $this['updated_at'],
       $this['expires_at'], $this['is_activated'],
@@ -42,6 +44,15 @@ class JobeetJobForm extends BaseJobeetJobForm
     
     $this->widgetSchema->setNameFormat('job[%s]');
 
+  }
+  
+  protected function removeFields()
+  {
+    unset(
+      $this['created_at'], $this['updated_at'],
+      $this['expires_at'], $this['is_activated'],
+      $this['token']
+    );
   }
 }
 ?>
