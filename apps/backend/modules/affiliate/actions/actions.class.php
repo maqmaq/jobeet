a2fd9ec2d0131ec92f005d09b111886d63912803
Maciej Maćkowski <mmackows@sigma.ug.edu.pl>
2011-08-22 17:47:36 +0200

    Day 16

--- apps/backend/modules/affiliate/actions/actions.class.php
+++ apps/backend/modules/affiliate/actions/actions.class.php
@@ -13,9 +13,27 @@ require_once dirname(__FILE__).'/../lib/affiliateGeneratorHelper.class.php';
  */
 class affiliateActions extends autoAffiliateActions
 {
-	  public function executeListActivate()
+
+  public function executeListActivate()
   {
-    $this->getRoute()->getObject()->activate();
+    $affiliate = $this->getRoute()->getObject();
+    $affiliate->activate();
+ 
+    // send an email to the affiliate
+    $message = $this->getMailer()->compose(
+      array('jobeet@sigma.ug.edu.pl' => 'Jobeet Bot'),
+      $affiliate->getEmail(),
+      'Jobeet affiliate token',
+      <<<EOF
+Your Jobeet affiliate account has been activated.
+ 
+Your token is {$affiliate->getToken()}.
+ 
+The Jobeet Bot.
+EOF
+    );
+ 
+    $this->getMailer()->send($message);
  
     $this->redirect('jobeet_affiliate');
   }
